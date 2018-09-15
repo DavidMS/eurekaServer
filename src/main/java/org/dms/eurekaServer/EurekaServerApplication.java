831affc7ee3a17034179ec87aad6a1030955a9c0
David <david.martinezsepulveda@plexus.com>
2018-09-16 00:22:18 +0200

    Initial Commit
    Initial Commit for eurekaServer

---
+++ src/main/java/org/dms/eurekaServer/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+/*
+ * @author: David Martinez Sepulveda
+ * @date: 15-sep-2018
+ * 
+ */
+package org.dms.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
